a6001b51a35a2f2bcbbb9b187de3b0ffabdd96a9
Leo <leolima539@gmail.com>
2026-01-23 16:36:25 -0800

    Fix Vite path aliases (@ and @shared)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   root: path.resolve("./client"),
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./client/src"),
+      "@shared": path.resolve("./shared"),
+    },
+  },
   build: {
     outDir: path.resolve("./dist/public"),
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
